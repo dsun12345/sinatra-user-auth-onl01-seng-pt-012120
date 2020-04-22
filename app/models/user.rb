8365cb6d75e09a4e173a706a8e5ff59e92bdc05b
Daniel Sun <danielsun12345@gmail.com>
2020-04-22 08:26:59 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,4 @@
 class User < ActiveRecord::Base
-  
-  
-  def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password
-    end 
-  end 
+
 
 end
